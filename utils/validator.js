38d5b2de0cb0cc3a1bcff53b94ccf1e596427fa8
Vishva <adharsh.scipy@gmail.com>
2025-11-18 15:30:53 +0530

    super admin 3

--- utils/validator.js
+++ utils/validator.js
@@ -1,23 +1,24 @@
 const emailValidator = (email) => {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 };
 
 const passwordValidator = (password) => {
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+[\]{}|;:',.<>?]).{8,64}$/;
-  return regex.test(password);
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+[\]{}|;:',.<>?]).{8,64}$/;
+    return regex.test(password);
 };
 
 const nameValidator = (name) => {
-  return typeof name === "string" && name.trim().length >= 2 && name.trim().length <= 50;
+    return typeof name === "string" && name.trim().length >= 2 && name.trim().length <= 50;
 };
 
 const phoneValidator = (phone) => {
-  phone = phone.replace(/[\s-()]/g, "");
+    phone = phone.replace(/[\s\-()]/g, "");
 
-  const regex = /^(?:\+971|00971|0)?(?:(?:50|52|54|55|56|58)\d{7} |(?:2|3|4|6|7|9)\d{7} |(?:600|800|900)\d{4,6})$/x;
+    const regex = /^(?:(?:\+?971|00971|0)?(?:50|52|54|55|56|58)\d{7} | (?:\+?971|00971|0)?(?:2|3|4|6|7|9)\d{7} | (?:600)\d{6} | (?:800)\d{3,6} | (?:900)\d{6})$/;
 
-  return regex.test(phone);
+    return regex.test(phone);
 };
+
 
 export { emailValidator, passwordValidator, nameValidator, phoneValidator };
